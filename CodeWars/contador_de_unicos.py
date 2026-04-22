a0543ed967b70b15bda9ece8d611475238459c7c
hollow <velpointl2@gmail.com>
2026-04-22 09:15:22 -0300

    Alteração no for do contador / Resolução de numeros divisiveis

--- CodeWars/contador_de_unicos.py
+++ CodeWars/contador_de_unicos.py
@@ -1,7 +1,7 @@
 from collections import Counter
-
 def find_unique(numbers: list):
     counts = Counter(numbers)
     for n in numbers:
         if counts[n] == 1:
             return n
+    return None
